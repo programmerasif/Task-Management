d650a193e84734c7634c8b0743fef5a403615b54
programmerasif <mdasifkhan3757@gmail.com>
2023-09-08 00:15:47 +0600

    first commit

---
+++ src/components/AllTask.jsx
@@ -0,0 +1,26 @@
+
+const AllTask = () => {
+    return (
+        <div>
+            <div className="bg-[#00000042]  rounded-md text-center h-full w-96 text-white">
+            <h5 className="text-2xl font-semibold border-b-2 border-[#00000079] py-5">All-Task</h5>
+            
+            <div className="flex flex-col">
+
+            <div className="w-72 px-5 mx-auto p-5 rounded-md mt-5 mb-5 hover:bg-[#00000032] bg-[#0000005f] duration-200">
+                <h5 className="text-start text-xl ">1. Learn Html</h5>
+            </div>
+            <div className="w-72 px-5 mx-auto p-5 rounded-md mt-5 mb-5 hover:bg-[#00000032] bg-[#0000005f] duration-200">
+                <h5 className="text-start text-xl ">1. Learn Html</h5>
+            </div>
+            <div className="w-72 px-5 mx-auto p-5 rounded-md mt-5 mb-5 hover:bg-[#00000032] bg-[#0000005f] duration-200">
+                <h5 className="text-start text-xl ">1. Learn Html</h5>
+            </div>
+            </div>
+            
+        </div>
+        </div>
+    );
+};
+
+export default AllTask;
---
+++ src/components/Doing.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Doing = () => {
+    return (
+        <div className="bg-[#00000042]  rounded-md text-center h-full w-96 text-white">
+            <h5 className="text-2xl font-semibold border-b-2 border-[#00000079] py-5">All-Task</h5>
+            
+            <div className="flex flex-col">
+
+            <div className="w-72 px-5 mx-auto p-5 rounded-md mt-5 mb-5 bg-[#0000005f]">
+                <h5 className="text-start text-xl ">1. Learn Html</h5>
+            </div>
+            <div className="w-72 px-5 mx-auto p-5 rounded-md mt-5 mb-5 bg-[#0000005f]">
+                <h5 className="text-start text-xl ">1. Learn Html</h5>
+            </div>
+            <div className="w-72 px-5 mx-auto p-5 rounded-md mt-5 mb-5 bg-[#0000005f]">
+                <h5 className="text-start text-xl ">1. Learn Html</h5>
+            </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Doing;
